feb1859d922a35f1b8f210f7c725c05ec47b607a
FLU <valerio.massj@gmail.com>
2022-01-21 14:26:53 +0100

    first commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,103 @@
+<footer>
+
+    <section id="footer-nav">
+    
+        <div class="container">
+    
+        <nav class="nav-container">
+    
+            <div class="left-block">
+    
+            <h3>DC comics</h3>
+    
+            <ul>
+                <li>a</li>
+                <li>b</li>
+                <li>c</li>
+                <li>d</li>
+                <li>e</li>
+                <li>f</li>
+                <li>g</li>
+            </ul>
+    
+            <h3 id="shop">SHOP</h3>
+    
+            <ul>
+                <li>a</li>
+                <li>b</li>
+            </ul>
+    
+            </div>
+    
+            <div class="mid-block">
+    
+            <h3>DC</h3>
+    
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
+                <li>7</li>
+                <li>8</li>
+                <li>9</li>
+                <li>10</li>
+                <li>11</li>
+            </ul>
+    
+            </div>
+    
+            <div class="right-block">
+    
+            <h3>Sites</h3>
+    
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+    
+            </div>
+    
+        </nav>
+    
+        <div class="image">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1200px-DC_Comics_logo.svg.png" alt="">
+        </div>
+    
+        </div>
+    
+    </section>
+
+    <section id="footer-socials">
+    
+        <div class="container">
+    
+          <div class="btn">
+            <a href="#">SIGN-UP NOW!</a>
+          </div>
+    
+          <div class="socials">
+    
+            <h3>FOLLOW US</h3>
+    
+            <div class="logo-socials">
+    
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+        
+            </div>
+    
+          </div>
+    
+        </div>
+    
+      </section>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,30 @@
+<header>
+  
+  <div class="logo">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/600px-DC_Comics_logo.png" alt="">
+  </div>
+
+  <nav>
+
+    <ul>
+
+      <li><a href="#">Characters</a></li>
+      <li><a href="#" class="active">Comics</a></li>
+      <li><a href="#">Movies</a></li>
+      <li><a href="#">TV</a></li>
+      <li><a href="#">Games</a></li>
+      <li><a href="#">Collectibles</a></li>
+      <li><a href="#">Videos</a></li>
+      <li><a href="#">Fans</a></li>
+      <li><a href="#">News</a></li>
+      <li><a href="#">Shop</a></li>
+      
+    </ul>
+
+  </nav> 
+
+</header>
+
+
+<div class="jumbo">
+</div>
